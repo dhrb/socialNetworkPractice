39607117e4f3b39420814c15924603a1303248e4
Dmitriy Horbatyuk <dmitriyhorbatyuk@Dmitriys-MacBook-Pro.local>
2021-09-30 13:55:43 +0300

    dir structure change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
-import Header from './Components/Header';
-import Navbar from './Components/Navbar';
-import Profile from './Components/Profile';
+import Header from './Components/Header/Header.jsx';
+import Navbar from './Components/Navbar/Navbar';
+import Profile from './Components/Profile/Profile';
 
 const App = () => {
         return (
             <div className='app-wrapper'>
                 <Header />
                 <Navbar />
-                <Profile />
+                <Profile /> 
             </div>
         )
     }
